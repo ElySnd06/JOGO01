e8b8560967e71f605153b31f55bfed4c7e3dc880
Ely Sande <elysande06@gmail.com>
2026-05-11 18:23:08 -0300

    Definindo a dimensão do palco do jogo

--- jogo.js
+++ jogo.js
@@ -1,1 +1,11 @@
-alert('teste')
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log (altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
